440507ca5d0a0b90a17c5b42e42746136fb37090
sierranapieralski <111990838+sierranapieralski@users.noreply.github.com>
2024-10-22 13:21:38 -0400

    progress!

--- C292MainProject/Assets/Scripts/Book.cs
+++ C292MainProject/Assets/Scripts/Book.cs
@@ -9,7 +9,10 @@ public class Book : MonoBehaviour
     [SerializeField] GameObject moveBook;
     Vector3 initialPosition;
 
+    public string color;
+
     bool isDragging = false;    // make public method and when is we drag to outline, then have book snap to correct position
+    [SerializeField] bool isDraggable = false;
 
 
     // Start is called before the first frame update
@@ -29,29 +32,40 @@ public class Book : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("In Book file on trigger enter 2d function");
-        Debug.Log("Collision detected with: " + collision.gameObject.name);
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Debug.Log("In Book file on trigger enter 2d function");
+    //    Debug.Log("Collision detected with: " + collision.gameObject.name);
 
-        if (collision.gameObject.tag == "EmptyBookSlot")   // If the player moves found book to collide with correct book outline,
-                                                           // place book in correct shelf spot, delete the outline and book so cant
-                                                           // be moved again, and increase player score of total books found
-        {
-            Debug.Log("Player moved book to empty book slot on shelf!");
-            //collision.gameObject.GetComponent<Player>().GrantImmunity(immunityDuration);
-            //Destroy(gameObject);  // Remove the book and book slot from the game after it's collected
-        }
+    //    if (collision.gameObject.tag == "EmptyBookSlot")   // If the player moves found book to collide with correct book outline,
+    //                                                       // place book in correct shelf spot, delete the outline and book so cant
+    //                                                       // be moved again, and increase player score of total books found
+    //    {
+    //        Debug.Log("Player moved book to empty book slot on shelf!");
+    //        //collision.gameObject.GetComponent<Player>().GrantImmunity(immunityDuration);
+    //        //Destroy(gameObject);  // Remove the book and book slot from the game after it's collected
+    //    }
         
-    }
+    //}
 
     private void OnMouseDown()
     {
-        isDragging = true;
+        if (isDraggable)
+        {
+            isDragging = true;
+        }
     }
 
     private void OnMouseUp()
     {
         isDragging = false;
+    }
+
+    public void snapBook() 
+    {
+        // stops being dragged by the mouse and is draggable false
+        isDragging = false;
+        isDraggable = false;
+
     }
 }
---
+++ C292MainProject/Assets/Scripts/Outline.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline : MonoBehaviour
+{
+
+    [SerializeField] string color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
+        if (collision.gameObject.tag == "MoveBook")
+        {
+            Debug.Log("is a book!");
+            if (collision.gameObject.GetComponent<Book>().color == color)
+            {
+                Debug.Log("matched color!");
+                // can check rotation
+
+                // take book and snap to match outlines position, when it gets moved over outline, snaps to shelf
+                collision.gameObject.GetComponent<Book>().snapBook();
+                // move book to correct position
+                collision.gameObject.transform.position = transform.position;
+
+            }
+        }
+    }
+}
